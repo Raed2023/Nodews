a32227cf134a0ccdd2aeaa2fbcfc452b0cb51651
Raed2023 <raedbenabdallah25@gamil.com>
2023-03-30 12:20:39 +0100

    nodews

--- Email-sender.js
+++ Email-sender.js
@@ -1,24 +1,64 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
+// const transporter = nodemailer.createTransport({
+//     service: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: 'raedbenabdallah25@gmail.com',
+//         pass: "qsau xock gjjj abip"
+
+//     }
+// });
+
+// const mailOptions = {
+//     from: 'raedbenabdallah25@gmail.com',
+//     to: 'yahyaimen971@gmail.com',
+//     subject: 'Test email',
+//     text: 'Hello from Node.js!'
+// };
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-        user: 'youremail@gmail.com',
-        pass: 'yourpassword'
-    }
-});
+      user: "raedbenabdallah25@gmail.com", // generated ethereal user
+      pass: "qsauxockgjjjabip", // generated ethereal password
+    },
+  });
 
-const mailOptions = {
-    from: 'youremail@gmail.com',
-    to: 'recipientemail@gmail.com',
-    subject: 'Test email',
-    text: 'Hello from Node.js!'
-};
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'raedbenabdallah25@gmail.com', // sender address
+    to: "yahyaimen971@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
